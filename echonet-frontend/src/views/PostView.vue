b4d27c7e77b38a9efedcbb75f2fd81324651cf8e
pokeplay04 <149372634+pokeplay04@users.noreply.github.com>
2025-05-07 19:56:07 +0200

    Cambios de Tailwind a Bootstrap
    No nos funciona Tailwind y por ese motivo hemos decidido cambiar el esitlo a bootstrap

--- echonet-frontend/src/views/PostView.vue
+++ echonet-frontend/src/views/PostView.vue
@@ -1,38 +1,36 @@
 <template>
-    <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-        <div class="main-center col-span-3 space-y-4">
-            <div 
-                class="p-4 bg-white border border-gray-200 rounded-lg"
-                v-if="post.id"
-            >
-                <FeedItem v-bind:post="post" />
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 mb-4">
+                <div v-if="post.id" class="p-3 bg-white border rounded mb-4">
+                    <FeedItem :post="post" />
+                </div>
+
+                <div
+                    v-for="comment in post.comments"
+                    :key="comment.id"
+                    class="p-3 bg-white border rounded mb-3 ms-4"
+                >
+                    <CommentItem :comment="comment" />
+                </div>
+
+                <div class="bg-white border rounded">
+                    <form @submit.prevent="submitForm" method="post">
+                        <div class="p-3">
+                            <textarea v-model="body" class="form-control" rows="4" placeholder="What do you think?"></textarea>
+                        </div>
+                        <div class="p-3 border-top">
+                            <button type="submit" class="btn btn-primary">Comment</button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
-            <div
-                class="p-4 ml-6 bg-white border border-gray-200 rounded-lg"
-                v-for="comment in post.comments"
-                v-bind:key="comment.id"
-            >
-                <CommentItem v-bind:comment="comment" />
+            <!-- Right sidebar -->
+            <div class="col-lg-3 mb-4">
+                <PeopleYouMayKnow />
+                <Trends class="mt-4" />
             </div>
-
-            <div class="bg-white border border-gray-200 rounded-lg">
-                <form v-on:submit.prevent="submitForm" method="post">
-                    <div class="p-4">  
-                        <textarea v-model="body" class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What do you think?"></textarea>
-                    </div>
-
-                    <div class="p-4 border-t border-gray-100">
-                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Comment</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <div class="main-right col-span-1 space-y-4">
-            <PeopleYouMayKnow />
-
-            <Trends />
         </div>
     </div>
 </template>
@@ -73,8 +71,6 @@ export default {
             axios
                 .get(`/api/posts/${this.$route.params.id}/`)
                 .then(response => {
-                    console.log('data', response.data)
-
                     this.post = response.data.post
                 })
                 .catch(error => {
@@ -83,15 +79,11 @@ export default {
         },
 
         submitForm() {
-            console.log('submitForm', this.body)
-
             axios
                 .post(`/api/posts/${this.$route.params.id}/comment/`, {
                     'body': this.body
                 })
                 .then(response => {
-                    console.log('data', response.data)
-
                     this.post.comments.push(response.data)
                     this.post.comments_count += 1
                     this.body = ''
