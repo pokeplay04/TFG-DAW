e0e0f064b02c4c6e47b7f2afc873bdbcbe99ea8e
pokeplay04 <149372634+pokeplay04@users.noreply.github.com>
2025-05-07 19:50:56 +0200

    Add files via upload

--- echonet-frontend/src/views/LoginView.vue
+++ echonet-frontend/src/views/LoginView.vue
@@ -1,41 +1,44 @@
 <template>
-    <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
-        <div class="main-left">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <h1 class="mb-6 text-2xl">Log in</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mb-4">
+                <div class="p-4 bg-white border rounded">
+                    <h1 class="mb-4 h4">Log in</h1>
 
-                <p class="mb-6 text-gray-500">
-                </p>
+                    <p class="mb-4 text-muted">
+                    </p>
 
-                <p class="font-bold">
-                    No tienes una cuenta? <RouterLink :to="{'name': 'signup'}" class="underline">Clica aquí</RouterLink> para crearte una!
-                </p>
+                    <p class="fw-bold">
+                        ¿No tienes una cuenta? 
+                        <RouterLink :to="{ name: 'signup' }" class="text-decoration-underline">Clica aquí</RouterLink> para crearte una!
+                    </p>
+                </div>
             </div>
-        </div>
 
-        <div class="main-right">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <form class="space-y-6" v-on:submit.prevent="submitForm">
-                    <div>
-                        <label>Email</label><br>
-                        <input type="email" v-model="form.email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-
-                    <div>
-                        <label>Password</label><br>
-                        <input type="password" v-model="form.password" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-
-                    <template v-if="errors.length > 0">
-                        <div class="bg-red-300 text-white rounded-lg p-6">
-                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            <div class="col-md-6 mb-4">
+                <div class="p-4 bg-white border rounded">
+                    <form @submit.prevent="submitForm">
+                        <div class="mb-3">
+                            <label class="form-label">Email</label>
+                            <input type="email" v-model="form.email" placeholder="Your e-mail address" class="form-control">
                         </div>
-                    </template>
 
-                    <div>
-                        <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Log in</button>
-                    </div>
-                </form>
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" v-model="form.password" placeholder="Your password" class="form-control">
+                        </div>
+
+                        <template v-if="errors.length > 0">
+                            <div class="alert alert-danger">
+                                <p v-for="error in errors" :key="error">{{ error }}</p>
+                            </div>
+                        </template>
+
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary">Log in</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
@@ -43,16 +46,12 @@
 
 <script>
 import axios from 'axios'
-
 import { useUserStore } from '@/stores/user'
 
 export default {
     setup() {
         const userStore = useUserStore()
-
-        return {
-            userStore
-        }
+        return { userStore }
     },
 
     data() {
@@ -64,6 +63,7 @@ export default {
             errors: []
         }
     },
+
     methods: {
         async submitForm() {
             this.errors = []
@@ -77,31 +77,19 @@ export default {
             }
 
             if (this.errors.length === 0) {
-                await axios
-                    .post('/api/login/', this.form)
-                    .then(response => {
-                        this.userStore.setToken(response.data)
+                try {
+                    const response = await axios.post('/api/login/', this.form)
+                    this.userStore.setToken(response.data)
+                    axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.access
 
-                        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.access;
-                    })
-                    .catch(error => {
-                        console.log('error', error)
+                    const userResponse = await axios.get('/api/me/')
+                    this.userStore.setUserInfo(userResponse.data)
+                    this.$router.push('/feed')
 
-                        this.errors.push('The email or password is incorrect! Or the user is not activated!')
-                    })
-            }
-            
-            if (this.errors.length === 0) {
-                await axios
-                    .get('/api/me/')
-                    .then(response => {
-                        this.userStore.setUserInfo(response.data)
-
-                        this.$router.push('/feed')
-                    })
-                    .catch(error => {
-                        console.log('error', error)
-                    })
+                } catch (error) {
+                    console.log('error', error)
+                    this.errors.push('The email or password is incorrect! Or the user is not activated!')
+                }
             }
         }
     }
--- echonet-frontend/src/views/SignupView.vue
+++ echonet-frontend/src/views/SignupView.vue
@@ -1,51 +1,54 @@
 <template>
-    <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
-        <div class="main-left">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <h1 class="mb-6 text-2xl">Sign up</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mb-4">
+                <div class="p-4 bg-white border rounded">
+                    <h1 class="mb-4 h4">Sign up</h1>
 
-                <p class="mb-6 text-gray-500">
-                </p>
+                    <p class="mb-4 text-muted">
+                    </p>
 
-                <p class="font-bold">
-                    Already have an account? <RouterLink :to="{'name': 'login'}" class="underline">Click here</RouterLink> to log in!
-                </p>
+                    <p class="fw-bold">
+                        ¿Ya tienes una cuenta? 
+                        <RouterLink :to="{ name: 'login' }" class="text-decoration-underline">Click here</RouterLink> to log in!
+                    </p>
+                </div>
             </div>
-        </div>
 
-        <div class="main-right">
-            <div class="p-12 bg-white border border-gray-200 rounded-lg">
-                <form class="space-y-6" v-on:submit.prevent="submitForm">
-                    <div>
-                        <label>Name</label><br>
-                        <input type="text" v-model="form.name" placeholder="Your full name" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-
-                    <div>
-                        <label>E-mail</label><br>
-                        <input type="email" v-model="form.email" placeholder="Your e-mail address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-
-                    <div>
-                        <label>Password</label><br>
-                        <input type="password" v-model="form.password1" placeholder="Your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-
-                    <div>
-                        <label>Repeat password</label><br>
-                        <input type="password" v-model="form.password2" placeholder="Repeat your password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg">
-                    </div>
-
-                    <template v-if="errors.length > 0">
-                        <div class="bg-red-300 text-white rounded-lg p-6">
-                            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            <div class="col-md-6 mb-4">
+                <div class="p-4 bg-white border rounded">
+                    <form @submit.prevent="submitForm">
+                        <div class="mb-3">
+                            <label class="form-label">Name</label>
+                            <input type="text" v-model="form.name" placeholder="Your full name" class="form-control">
                         </div>
-                    </template>
 
-                    <div>
-                        <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign up</button>
-                    </div>
-                </form>
+                        <div class="mb-3">
+                            <label class="form-label">E-mail</label>
+                            <input type="email" v-model="form.email" placeholder="Your e-mail address" class="form-control">
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label">Password</label>
+                            <input type="password" v-model="form.password1" placeholder="Your password" class="form-control">
+                        </div>
+
+                        <div class="mb-3">
+                            <label class="form-label">Repeat password</label>
+                            <input type="password" v-model="form.password2" placeholder="Repeat your password" class="form-control">
+                        </div>
+
+                        <template v-if="errors.length > 0">
+                            <div class="alert alert-danger">
+                                <p v-for="error in errors" :key="error">{{ error }}</p>
+                            </div>
+                        </template>
+
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary">Sign up</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
@@ -53,16 +56,12 @@
 
 <script>
 import axios from 'axios'
-
 import { useToastStore } from '@/stores/toast'
 
 export default {
     setup() {
         const toastStore = useToastStore()
-
-        return {
-            toastStore
-        }
+        return { toastStore }
     },
 
     data() {
@@ -110,7 +109,7 @@ export default {
                             this.form.password2 = ''
                         } else {
                             const data = JSON.parse(response.data.message)
-                            for (const key in data){
+                            for (const key in data) {
                                 this.errors.push(data[key][0].message)
                             }
 
