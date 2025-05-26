3208499f6d7accf73228d3bcf37ba6f6346a3851
Meggie <161296223+Megigi7@users.noreply.github.com>
2025-05-26 12:30:07 +0200

    Delete account directory

--- account/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-from .models import SpotifyUser
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = SpotifyUser
-        fields = ('display_name', 'avatar')
