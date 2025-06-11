8bffa82eb9bff765af8bfe91f22d37a7169fcc1d
pokeplay04 <149372634+pokeplay04@users.noreply.github.com>
2025-06-11 22:01:27 +0200

    Navbar responsive

--- echonet-frontend/src/App.vue
+++ echonet-frontend/src/App.vue
@@ -1,55 +1,72 @@
 <template>
-  <div>
-    <!-- Navbar con fondo oscuro -->
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1a1a1a;">
+  <div class="min-vh-100" style="background-color: #100036;">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light border-bottom py-0 px-4" style="background-color: #350775;">
       <div class="container-fluid">
-        <a class="navbar-brand fw-bold fs-4 text-white" href="#">Echonet</a>
+        <a class="navbar-brand fw-bold fs-4 text-white" href="/feed">
+          <img src="@/assets/logo.png" alt="Echonet Logo" width="150px" height="20%" class="d-inline-block align-text-top logo">
+        </a>
+
         <div class="d-none d-lg-flex gap-4 mx-auto" v-if="userStore.user.isAuthenticated">
+          <!-- HOME -->
           <RouterLink to="/feed" class="nav-link text-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-house-door" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
-                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875
-                       c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125
-                       c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#39ff65" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M5 12h-2l9 -9l9 9h-2" />
+              <path d="M5 12v7a2 2 0 0 0 2 2h5" />
+              <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2" />
+              <path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5" />
             </svg>
           </RouterLink>
-          <RouterLink to="/chat" class="nav-link text-light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125
-                       0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125
-                       0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5
-                       3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283
-                       3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332
-                       48.294 48.294 0 005.83-.498c1.585-.233
-                       2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394
-                       48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373
-                       3.746 2.25 5.14 2.25 6.741v6.018z"/>
+
+          <!-- DESCUBRIR MUSICA -->
+          <RouterLink to="/discover" class="nav-link text-light">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#39ff65" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+              <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+              <path d="M7 12a5 5 0 0 1 5 -5" />
+              <path d="M12 17a5 5 0 0 0 5 -5" />
             </svg>
           </RouterLink>
-          <RouterLink to="/notifications" class="nav-link text-light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967
-                       8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967
-                       8.967 0 01-2.312 6.022c1.733.64 3.56 1.085
-                       5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714
-                       0a3 3 0 11-5.714 0"/>
-            </svg>
+
+          <!-- NOTIFICACIONES -->
+          <RouterLink to="/notifications" class="nav-link text-light position-relative">
+            <span v-if="hasUnreadNotifications" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="font-size: 0.5rem; padding: 0.25em 0.4em;">!</span>
+              <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#39ff65" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+                <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+              </svg>
           </RouterLink>
+
+          <!-- BUSCAR -->
           <RouterLink to="/search" class="nav-link text-light">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="24" height="24">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0
-                       105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#39ff65" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M3 17a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+              <path d="M9 17v-13h10v7" />
+              <path d="M9 8h10" />
+              <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+              <path d="M20.2 20.2l1.8 1.8" />
+            </svg>
+          </RouterLink>
+
+          <!-- CHAT -->
+          <RouterLink to="/chat" class="nav-link text-light">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#39ff65" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
+              <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
             </svg>
           </RouterLink>
         </div>
+
         <div class="d-flex align-items-center gap-3">
           <template v-if="userStore.user.isAuthenticated && userStore.user.id">
             <RouterLink :to="{ name: 'profile', params: { id: userStore.user.id } }">
-              <img :src="userStore.user.avatar" class="rounded-circle" style="width: 48px; height: 48px;" />
-            </RouterLink>
+            <img
+              v-if="userStore.user.avatar"
+              :src="getAvatarUrl(userStore.user.avatar)"
+              class="rounded-circle"
+              style="width: 48px; height: 48px;"
+            />            
+          </RouterLink>
           </template>
           <template v-else>
             <RouterLink to="/signup" class="btn btn-primary">Acceder</RouterLink>
@@ -58,7 +75,7 @@
       </div>
     </nav>
 
-    <main class="container py-4">
+    <main class="container py-2">
       <RouterView />
     </main>
 
@@ -70,7 +87,7 @@
 import axios from 'axios'
 import Toast from '@/components/Toast.vue'
 import { useUserStore } from '@/stores/user'
-import { onBeforeMount } from 'vue'
+import { onBeforeMount, onMounted, ref, onBeforeUnmount } from 'vue'
 
 export default {
   components: {
@@ -78,15 +95,44 @@ export default {
   },
   setup() {
     const userStore = useUserStore()
+    const hasUnreadNotifications = ref(false)
+    let pollInterval = null
+
+    const getNotifications = () => {
+      axios
+        .get('api/notifications/')
+        .then(response => {
+          hasUnreadNotifications.value = response.data.length > 0
+        })
+        .catch(error => {
+          console.log('Error al obtener notificaciones:', error)
+        })
+    }
 
     onBeforeMount(() => {
       userStore.initStore()
       const token = userStore.user.access
       axios.defaults.headers.common["Authorization"] = token ? "Bearer " + token : ""
+      getNotifications()
+    })
+
+    onMounted(() => {
+      // Actualiza cada 30 segundos
+      pollInterval = setInterval(getNotifications, 5000)
+    })
+
+    onBeforeUnmount(() => {
+      clearInterval(pollInterval)
     })
 
     return {
-      userStore
+      userStore,
+      hasUnreadNotifications
+    }
+  },
+  methods: {
+    getAvatarUrl(path) {
+      return path.startsWith('http') ? path : `http://localhost:8000${path}` // BACKEND_URL
     }
   }
 }
