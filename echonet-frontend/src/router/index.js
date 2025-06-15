501a0d3abdac2f01802d0a09bb480e8e877aef52
pokeplay04 <149372634+pokeplay04@users.noreply.github.com>
2025-06-15 19:56:43 +0200

    Add files via upload

--- echonet-frontend/src/router/index.js
+++ echonet-frontend/src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SignupView from '@/views/SignupView.vue'
+import HomeView from '@/views/HomeView.vue'
 import FeedView from '../views/FeedView.vue'
-import SignupView from '../views/SignupView.vue'
-import LoginView from '../views/LoginView.vue'
 import SearchView from '../views/SearchView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import FriendsView from '../views/FriendsView.vue'
@@ -10,84 +9,88 @@ import PostView from '../views/PostView.vue'
 import ChatView from '../views/ChatView.vue'
 import TrendView from '../views/TrendView.vue'
 import EditProfileView from '../views/EditProfileView.vue'
-import EditPasswordView from '../views/EditPasswordView.vue'
 import NotificationsView from '../views/NotificationsView.vue'
+import DiscoverMusic from '@/views/DiscoverMusic.vue'
+
+const routes = [
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignupView
+  },
+  {
+    path: '/',
+    name: 'SignupRedirect',
+    redirect: '/signup',
+  },
+  {
+    path: '/feed',
+    name: 'feed',
+    component: FeedView
+  },
+  {
+    path: '/discover',
+    name: 'discover',
+    component: DiscoverMusic
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchView
+  },
+  {
+    path: '/chat',
+    name: 'chat',
+    component: ChatView
+  },
+  {
+    path: '/notifications',
+    name: 'notifications',
+    component: NotificationsView
+  },
+  {
+    path: '/profile/edit',
+    name: 'editprofile',
+    component: EditProfileView
+  },
+  {
+    path: '/profile/:id',
+    name: 'profile',
+    component: ProfileView
+  },
+  {
+    path: '/profile/:id/friends',
+    name: 'friends',
+    component: FriendsView
+  },
+  {
+    path: '/:id',
+    name: 'postview',
+    component: PostView
+  },
+  {
+    path: '/trends/:id',
+    name: 'trendview',
+    component: TrendView
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/feed',
-      name: 'feed',
-      component: FeedView
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: SignupView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: SearchView
-    },
-    {
-      path: '/chat',
-      name: 'chat',
-      component: ChatView
-    },
-    {
-      path: '/notifications',
-      name: 'notifications',
-      component: NotificationsView
-    },
-    {
-      path: '/profile/edit',
-      name: 'editprofile',
-      component: EditProfileView
-    },
-    {
-      path: '/profile/edit/password',
-      name: 'editpassword',
-      component: EditPasswordView
-    },
-    {
-      path: '/profile/:id',
-      name: 'profile',
-      component: ProfileView
-    },
-    {
-      path: '/profile/:id/friends',
-      name: 'friends',
-      component: FriendsView
-    },
-    {
-      path: '/:id',
-      name: 'postview',
-      component: PostView
-    },
-    {
-      path: '/trends/:id',
-      name: 'trendview',
-      component: TrendView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes
+})
+
+// Guard global de autenticación
+router.beforeEach((to, from, next) => {
+  // Aquí asume que el token de autenticación está en localStorage
+  const isAuthenticated = !!localStorage.getItem('user.access')
+  if (!isAuthenticated && to.path !== '/signup') {
+    next('/signup')
+  } else if (isAuthenticated && to.path === '/signup') {
+    next('/') // Opcional: redirige a home si ya está autenticado
+  } else {
+    next()
+  }
 })
 
 export default router
